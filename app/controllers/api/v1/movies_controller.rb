432be0026a814894cea11998dc70454f56608bad
Alora Riley <alora.riley@gmail.com>
2024-12-04 00:45:45 -0500

    Add serializer

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::MoviesController < ApplicationController
 
+  def index
+    render json: MovieSerializer.format_movie_info(Movie.all)
+  end
+
   def movie_params
     params.permit(:title, vote_average)
   end
