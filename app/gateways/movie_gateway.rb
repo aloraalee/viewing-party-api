671542dda6eba68cc421dfd2e1193d28d86bb86f
Alora Riley <alora.riley@gmail.com>
2024-12-09 14:37:12 -0700

    refactor: Create helper method for conn from Faraday

--- app/gateways/movie_gateway.rb
+++ app/gateways/movie_gateway.rb
@@ -1,25 +1,22 @@
 class MovieGateway
   def self.get_top_rated
-    conn = Faraday.new(
-      url: 'https://api.themoviedb.org/3',
-      params: {api_key: Rails.application.credentials.tmdb[:key]}
-    )
-
     response = conn.get("movie/top_rated") 
-
     JSON.parse(response.body)["results"].first(20)
   end
 
   def self.search_movies_by_keyword(search_term)
+    response = conn.get("search/movie") do |req| 
+      req.params["query"] = search_term
+    end
+    JSON.parse(response.body)["results"].first(20)
+  end
+
+  private
+
+  def self.conn
     conn = Faraday.new(
       url: 'https://api.themoviedb.org/3',
       params: {api_key: Rails.application.credentials.tmdb[:key]}
     )
-
-    response = conn.get("search/movie") do |req| 
-      req.params["query"] = search_term
-
-    end
-    JSON.parse(response.body)["results"].first(20)
   end
 end
