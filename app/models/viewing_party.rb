cce845c92f7e1ef8bb43db37b65d00aa106cb43a
Alora Riley <alora.riley@gmail.com>
2024-12-08 18:59:05 -0700

    feat: Add validations to viewing_party class

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,4 +1,10 @@
 class ViewingParty < ApplicationRecord
   has_many :attendees
   has_many :users, through: :attendees
+  has_many :invitees, through: :attendees, source: :user
+  validates :name, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :movie_id, presence: true
+  validates :movie_title, presence: true
 end
--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -4,5 +4,14 @@ RSpec.describe ViewingParty, type: :model do
   describe "relationships" do 
     it {should have_many :attendees}
     it {should have_many(:users) .through(:attendees)}
+    it {should have_many(:invitees) .through(:attendees) .source(:user)}
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:start_time) }
+    it { should validate_presence_of(:end_time) }
+    it { should validate_presence_of(:movie_id) }
+    it { should validate_presence_of(:movie_title) }
   end
 end
