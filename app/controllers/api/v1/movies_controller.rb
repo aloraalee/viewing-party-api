393a7d92886671ce04a547a9f9e7a23d694ec715
Alora Riley <alora.riley@gmail.com>
2024-12-26 20:42:50 -0500

    refactor: Move the search method in movies controller to the index action to make the controller slim.

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,19 +1,18 @@
 class Api::V1::MoviesController < ApplicationController
 
   def index
-    top_rated_movies = MovieGateway.get_top_rated
-    render json: MovieSerializer.format_movie_info(top_rated_movies)
+    if movie_id = params[:id]
+      movie_details = MovieGateway.get_movie_details(movie_id)
+      render json: MovieSerializer.format_movie_details(movie_details)
+    else
+      top_rated_movies = MovieGateway.get_top_rated
+      render json: MovieSerializer.format_movie_info(top_rated_movies)
+    end
   end
 
   def search
     search_term = params[:query]
     search_movies = MovieGateway.search_movies_by_keyword(search_term)
     render json: MovieSerializer.format_movie_info(search_movies)
-  end
-
-  def detail
-    movie_id = params[:id]
-    movie_details = MovieGateway.get_movie_details(movie_id)
-    render json: MovieSerializer.format_movie_details(movie_details)
   end
 end
